c85393506292a09dbd1bb474598af078ba487b51
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-06-03 02:56:04 -0300

    ajustes em todas as telas

--- index.js
+++ index.js
@@ -14,22 +14,3 @@ function fazerLogin() {
         window.location.href = "telaCurso.html"
     }
 }
-
-function subirImg() {
-    document.getElementById("inputSubirImg").click()
-}
-
-function previewImg(event) {
-    let imgCurso = document.getElementById("imgCurso")
-    imgCurso.src = URL.createObjectURL(event.target.files[0])
-
-    let btnSubirImg = document.getElementById("btnSubirImg")
-
-    btnSubirImg.classList.remove("btnSubirIngStyle")
-    btnSubirImg.classList.add("displayNone")
-
-    let divBtnSubirImg = document.getElementById("divBtnSubirImg")
-    
-    divBtnSubirImg.classList.remove("divBtnSubirImgStyle")
-    divBtnSubirImg.classList.add("borderNone")
-}
